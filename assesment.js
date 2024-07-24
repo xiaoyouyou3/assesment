2043c1e077dba5f96b8b762b0f59206715596254
xiaoyouyou3 <xiaolinyousi3@gmail.com>
2024-07-24 21:40:52 +0900

    更新

--- assesment.js
+++ assesment.js
@@ -15,14 +15,28 @@ assesmentButton.addEventListener(
 
         //診断結果表示エリアの作成
         resultDivision.innerText = '';
-        const header = document.createElement('h3');
-        header.innerText = '診断結果';
-        resultDivision.appendChild(header);
+        
+        //headerDivisionの作成
+        const headerDivision = document.createElement('div');
+        headerDivision.setAttribute('class', 'card-header text-bg-primary');
+        headerDivision.innerText = '診断結果';
+
+        //bodyDivisionの作成
+        const bodyDivision= document.createElement('div');
+        bodyDivision.setAttribute('class', 'card-body');
 
         const paragraph = document.createElement('p');
+        paragraph.setAttribute('class', 'card-text');
         const result = assesment(userName);
         paragraph.innerText = result;
-        resultDivision.appendChild(paragraph);
+        bodyDivision.appendChild(paragraph);
+
+        //resultDivision に Bootstrap のスタイルを適用する
+        resultDivision.setAttribute('class', 'card');
+
+        //headDivision と bodyDivision を resultDivision に差し込む
+        resultDivision.appendChild(headerDivision);
+        resultDivision.appendChild(bodyDivision);
 
         //ツイートエリアの作成
         tweetDivision.innerText = '';
